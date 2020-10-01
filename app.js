47ab860d162911082b3f0c29079653767e30682f
Shtever <Shtevetm@gmail.com>
2020-10-01 15:02:02 -0500

    Initial commit 2. Added assets, links, minor styling.

---
+++ app.js
@@ -0,0 +1,23 @@
+var goal = 555;
+var count = 1;
+var x = "";
+
+// Generate Random Number 
+function random() {
+//     x = (Math.floor(Math.random() * 1000) + 1);
+//     console.log(x);
+//     check();
+// }
+
+// // Check if random number=goal number
+// function check() {
+//     if (x !== goal) {
+//         x = (Math.floor(Math.random() * 1000) + 1);
+//         count++;
+//         random()
+//     }
+//     else {
+//         console.log("Ya made it to " + goal + " in " + count + " tries")
+//         alert("Ya made it to " + goal + " in " + count + " tries")
+//     }
+} 
